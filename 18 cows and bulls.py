92468eedd6dd14f1323e170cf8d8b919219d5182
Nozzin <gknozzin@gmail.com>
2016-12-02 19:36:17 -0500

    Add files via upload
    Took about three and a half hours

---
+++ 18 cows and bulls.py
@@ -0,0 +1,78 @@
+import random
+import sys
+
+def main():
+    
+    print('----------------------')
+    print('Rules:')
+    print('----------------------')
+    print('For every digit that the user guessed correctly in the correct place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” Every time the user makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.')
+    print('----------------------')
+    
+    number = get_number()
+    #user_number = ask_user()
+    #print(number)
+    number = ''.join(str(e) for e in number)
+    #print(number)
+    #print(user_number)
+   
+    cow = 0
+    bull = 0
+    guesses = 0
+    
+    while cow < 4:
+        user_number = ask_user()
+        
+        cow = 0
+        bull = 0
+        for i in range(0,4):
+            
+            #print(user_number[i])
+            
+            if number[i] == user_number[i]:
+                cow += 1
+
+            if number[i] != user_number[i]:
+                if number[i] == user_number[0]:
+                    bull+=1
+                elif number[i] == user_number[1]:
+                    bull+=1
+                elif number[i] == user_number[2]:
+                    bull+=1
+                elif number[i] == user_number[3]:
+                    bull+=1
+            
+        print('There were ',cow,'cows and ',bull,' bulls')
+        print('')
+        guesses+=1
+        #user_number = ask_user()
+    if cow == 4:
+        print('You win.')
+        print('It took you ',guesses,' guesses.')
+        sys.exit()
+        
+    
+
+def get_number():
+    random_number = []
+    #Get the 4 digit number for the game
+    for i in range(4):
+        num = random.randint(0,9)
+        random_number.append(num)
+
+    return random_number
+
+def ask_user():
+    #Get the 4 digit number from the user
+    user_number = input('Enter a 4 digit number: ')
+    stringNumber = str(user_number)
+    
+    return stringNumber
+
+            
+    
+
+
+
+
+main()
